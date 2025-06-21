1d10ebdb11339599b747ae82ba1c3428b1f556e9
Dharma Anargya Jowandy <16523104@std.stei.itb.ac.id>
2025-06-22 05:51:19 +0700

    fix : perbaikan algoritma pelanggaran preferensi dan menampilkan output

--- src/anomali_preferensi.c
+++ src/anomali_preferensi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "anomali_preferensi.h"
 #include "common.h"
 #include "Schedule.h"
@@ -18,23 +19,19 @@ void fill_empty_shift(struct shift_slot *slot, struct Doctor_data *head){
     for (i=0; i<90; i++){
         int week = (slot[i].date.date) / 7;
         if (slot[i].assigned_amount==0){
-            backup_doc=nonpreferensi_tersedikit(slot, head, i, week);
-            if (backup_doc!=NULL){
-                pelanggaranPerweek[week]++;
-                slot[i].assigned_doctor_ID[0] = backup_doc->ID;
-                slot[i].assigned_amount = 1;
-                backup_doc->assignedShiftsPerWeek[week]++;
-                backup_doc->totalAssignedShifts++;
-            }
-            else{
-                printf("tidak ada dokter yang tersedia untuk shift kosong\n");
+            int week = (slot[i].date.date) / 7;
+            struct Pelanggaran Hasil = nonpreferensi_tersedikit(slot, head, i, week);
+            if (Hasil.choosen_doctor != NULL){
+                int amount = slot[i].assigned_amount;
+                slot[i].List_pelanggaran[amount] = Hasil;
+                slot[i].assigned_doctor_ID[0] = Hasil.choosen_doctor->ID;
+                slot[i].assigned_amount ++;
+                Hasil.choosen_doctor->assignedShiftsPerWeek[week]++;
+                Hasil.choosen_doctor->totalAssignedShifts++;
             }
         }
 
     }
-    for(int j=1; j<5;j++){
-        printf("Jumlah pelanggaran preferensi pada minggu ke - %d: %d\n\n", j, pelanggaranPerweek[j]);
-    } 
 }
 
 int jumlah_dokter(struct Doctor_data *head){
@@ -47,51 +44,77 @@ int jumlah_dokter(struct Doctor_data *head){
     return jumlah;
 }
 
-struct Doctor_data *nonpreferensi_tersedikit(struct shift_slot *slot, struct Doctor_data *head, int which_shift, int week){
-    int total_dokter;
+struct Pelanggaran nonpreferensi_tersedikit(struct shift_slot *slot, struct Doctor_data *head, int which_shift, int week){
+    struct Pelanggaran hasil;
+    struct Doctor_data *fewest_nonprefer_doctor=NULL;
+    hasil.choosen_doctor = NULL;
+    hasil.Pelanggaran_preferensi = false;
+    hasil.Pelanggaran_cuti = false;
+    hasil.Pelanggaran_max_shift = false;
+    int *nonpreference_individu; // berapa preferensi yang telah dilanggar per individu jika dia dimasukkan di shift itu
     int doc_count;
     int fewest_nonprefer_val=max;
-    int *nonpreference_individu; // berapa preferensi yang telah dilanggar per individu jika dia dimasukkan di shift itu
     struct Doctor_data *traverser=head;
-    struct Doctor_data *fewest_nonprefer_doctor=NULL;
-    total_dokter=jumlah_dokter(head);
-    nonpreference_individu=(int*)malloc(total_dokter*sizeof(int));
+    
+
+    int total_dokter=jumlah_dokter(head);
+    if(total_dokter == 0){
+        printf("tidak ada dokter dalam daftar\n");
+        return hasil;
+    }
+
+    nonpreference_individu=(int*)calloc(total_dokter,sizeof(int));
+
     if (nonpreference_individu==NULL){
         printf("tidak ada dokter\n");
-        return NULL;
+        return hasil;
     }
-    memset(nonpreference_individu,0,total_dokter*sizeof(int));
+    
     doc_count=0;
     while (traverser!=NULL){
-        // set dokter yang sudah ada di shift tersebut ke high value
-        for (int doctor_id=0; doctor_id<11; doctor_id++){
-            if (slot[which_shift].assigned_doctor_ID[doctor_id]==traverser->ID){
-                nonpreference_individu[doc_count]=max;
-            }
-        }
-        if (nonpreference_individu[doc_count]!=max){
-            // hitung jumlah nonpreferensi yang terjadi untuk seorang dokter jika dimasukkan di shift itu 
-            if((traverser->prefersShift[slot[which_shift].shift] == 0)){
-                nonpreference_individu[doc_count]++;
-            }
-            if((traverser->assignedShiftsPerWeek[week]) >= (traverser->maxShiftsPerWeek)){
-                nonpreference_individu[doc_count]++;
-            }
-            if(check_assigned_status(traverser ->ID,slot, which_shift)==0){
-                nonpreference_individu[doc_count]++;            
-            }
-            if((slot[which_shift].date.date == traverser ->restDay)){
-                nonpreference_individu[doc_count]++;            
-            }
+
+        //Agar pelanggaran tidak melebihi 7 shift setiap minggu
+        bool cast_away = false;
+
+        if(traverser->assignedShiftsPerWeek[week] >= ABSOLUT || check_assigned_status(traverser->ID,slot, which_shift) == 0 ){
+            cast_away = true;
         }
 
-            // catat nilai jumlah nonpreferensi terkecil
-            if (fewest_nonprefer_val>nonpreference_individu[doc_count]){
-                fewest_nonprefer_val=nonpreference_individu[doc_count];
+        
+        if (cast_away){
+            nonpreference_individu[doc_count] = max;
+        }
+        else{
+            //apabila tidak pada preferensi shift pagi siang atau malam  diberi penalti 1
+            if((traverser->prefersShift[slot[which_shift].shift] == 0)){
+                nonpreference_individu[doc_count]+= 1;
             }
+            //apabila melebihi maximum shift per minggu diberi penalti 3
+            if((traverser->assignedShiftsPerWeek[week]) > (traverser->maxShiftsPerWeek)){
+                nonpreference_individu[doc_count]+= 3;
+            }
+            // Apabila hari libur diberi penalti 10
+            if((slot[which_shift].date.date == traverser ->restDay)){
+                nonpreference_individu[doc_count]+= 10;            
+            }
+
+        }
+        // set dokter yang sudah ada di shift tersebut ke high value
+        
+        // catat nilai jumlah nonpreferensi terkecil
+        if (fewest_nonprefer_val>nonpreference_individu[doc_count]){
+            fewest_nonprefer_val=nonpreference_individu[doc_count];
+        }
         traverser=traverser->next;
         doc_count++;
     }
+
+
+    if (fewest_nonprefer_val == max) {
+        free(nonpreference_individu);
+        return hasil;
+    }
+
     traverser=head;
     doc_count=0;
     while (traverser!=NULL){
@@ -102,6 +125,21 @@ struct Doctor_data *nonpreferensi_tersedikit(struct shift_slot *slot, struct Doc
         doc_count++;
         traverser=traverser->next;
     }
+
+    if (fewest_nonprefer_doctor != NULL)
+    {
+        hasil.choosen_doctor = fewest_nonprefer_doctor;
+        if(slot[which_shift].date.date == fewest_nonprefer_doctor->restDay){
+            hasil.Pelanggaran_cuti = true;
+        }
+        if(fewest_nonprefer_doctor ->prefersShift[slot[which_shift].shift] == 0){
+            hasil.Pelanggaran_preferensi = true;
+        }
+        if(fewest_nonprefer_doctor ->assignedShiftsPerWeek[week] > fewest_nonprefer_doctor ->maxShiftsPerWeek){
+            hasil.Pelanggaran_max_shift = true;
+        }
+    }
+    
     free(nonpreference_individu);
-    return fewest_nonprefer_doctor;
+    return hasil;
 }
--- src/anomali_preferensi.h
+++ src/anomali_preferensi.h
@@ -1,8 +1,9 @@
 #ifndef ANOMALI_PREFERENSI_H
 #define ANOMALI_PREFERENSI_H
+
 #include "common.h"
 
 void fill_empty_shift(struct shift_slot *slot, struct Doctor_data *head);
 int jumlah_dokter(struct Doctor_data *head);
-struct Doctor_data *nonpreferensi_tersedikit(struct shift_slot *slot, struct Doctor_data *head, int which_shift, int week);
+struct Pelanggaran nonpreferensi_tersedikit(struct shift_slot *slot, struct Doctor_data *head, int which_shift, int week);
 #endif
