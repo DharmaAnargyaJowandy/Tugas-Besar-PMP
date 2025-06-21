1d3d4ae7eb37d04de1ae8aaa74feafe6b1387682
Dharma Anargya Jowandy <16523104@std.stei.itb.ac.id>
2025-06-22 05:49:45 +0700

    feat : penambahan struct

--- src/common.h
+++ src/common.h
@@ -1,9 +1,11 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdbool.h>
 #define max 9999999
 #define FILENAME "daftar_dokter.csv"
 #define MAX_LINE 256
+#define ABSOLUT 7
 
 typedef enum { PAGI = 0, SIANG = 1, MALAM = 2 } ShiftType;
 
@@ -13,11 +15,21 @@ struct Date{
     int year;
 };
 
+struct Pelanggaran
+{
+    struct Doctor_data *choosen_doctor;
+    bool Pelanggaran_max_shift;
+    bool Pelanggaran_cuti;
+    bool Pelanggaran_preferensi;
+};
+
+
 struct shift_slot{
     struct Date date;
     ShiftType shift;
     int assigned_amount;
     int assigned_doctor_ID[4];
+    struct Pelanggaran List_pelanggaran[4];
 };
 
 struct Doctor_data{
