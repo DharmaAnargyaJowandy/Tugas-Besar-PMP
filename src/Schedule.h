6fc8bf5528a803ca3e3d7e1a8e71fab5ed7b5c1c
bryanpurba1711 <13223065@std.stei.itb.ac.id>
2025-06-22 15:57:24 +0700

    Menambah library kelola menu tampil jadwal

--- src/Schedule.h
+++ src/Schedule.h
@@ -8,6 +8,7 @@ void print_schedule(struct shift_slot *Slots, struct Doctor_data *front );
 bool get_integer_input(const char* prompt, int* result);
 void print_schedule_for_day(struct shift_slot *Slots, struct Doctor_data *front, int day);
 void print_schedule_for_week(struct shift_slot *Slots, struct Doctor_data *front, int week);
+void kelola_menu_tampil_jadwal(struct shift_slot *slot, struct Doctor_data *head, int jadwal_dibuat);
 int check_assigned_status(int doctor_ID, struct shift_slot *Slots, int current_index);
 int prevent_duplicate(int doctor_ID, struct shift_slot *Slots);
 void print_unassigned (struct Doctor_data * front);
